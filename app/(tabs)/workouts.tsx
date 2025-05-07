dfd49d8e4a7223310ee6079d6ba1882cae47e0b1
adherb <41672441+adherb@users.noreply.github.com>
2025-05-07 21:23:30 +1000

    WIP

---
+++ app/(tabs)/workouts.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function WorkoutsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Workouts Screen</Text>
+    </View>
+  );
+}
