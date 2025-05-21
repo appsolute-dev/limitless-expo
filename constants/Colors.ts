ebd9a50afd267e41b9dac7e1740eab7cb9132fb4
adherb <41672441+adherb@users.noreply.github.com>
2025-05-21 16:41:24 +1000

    WIP

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2DC7EA';
-const tintColorDark = '#fff';
+const tintColorLight = "#2DC7EA";
+const tintColorDark = "#2DC7EA";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
 };
